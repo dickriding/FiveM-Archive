7dc15ac21279938b5d0fa64f6c2a396e2ea80351
Icmp <118371667+dickriding@users.noreply.github.com>
2023-04-12 14:09:08 +0100

    (Add): Utilities

---
+++ Utilities/Resource-Dumper/dumper/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace dumper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ip = new Bunifu.Framework.UI.BunifuMetroTextbox();
+            this.port = new Bunifu.Framework.UI.BunifuMetroTextbox();
+            this.bunifuFlatButton1 = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.logi = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.bunifuCustomLabel6 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.bunifuCustomLabel5 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.bunifuCustomLabel4 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.bunifuCustomLabel3 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.bunifuCustomLabel1 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.bunifuCustomLabel2 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ip
+            // 
+            this.ip.BorderColorFocused = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ip.BorderColorIdle = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ip.BorderColorMouseHover = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ip.BorderThickness = 3;
+            this.ip.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.ip.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.ip.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ip.isPassword = false;
+            this.ip.Location = new System.Drawing.Point(13, 24);
+            this.ip.Margin = new System.Windows.Forms.Padding(4);
+            this.ip.Name = "ip";
+            this.ip.Size = new System.Drawing.Size(273, 44);
+            this.ip.TabIndex = 0;
+            this.ip.Text = "IP";
+            this.ip.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // port
+            // 
+            this.port.BorderColorFocused = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.port.BorderColorIdle = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.port.BorderColorMouseHover = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.port.BorderThickness = 3;
+            this.port.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.port.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.port.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.port.isPassword = false;
+            this.port.Location = new System.Drawing.Point(294, 24);
+            this.port.Margin = new System.Windows.Forms.Padding(4);
+            this.port.Name = "port";
+            this.port.Size = new System.Drawing.Size(95, 44);
+            this.port.TabIndex = 1;
+            this.port.Text = "PORT";
+            this.port.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // bunifuFlatButton1
+            // 
+            this.bunifuFlatButton1.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.bunifuFlatButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.bunifuFlatButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuFlatButton1.BorderRadius = 0;
+            this.bunifuFlatButton1.ButtonText = "Dump";
+            this.bunifuFlatButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bunifuFlatButton1.DisabledColor = System.Drawing.Color.Gray;
+            this.bunifuFlatButton1.Iconcolor = System.Drawing.Color.Transparent;
+            this.bunifuFlatButton1.Iconimage = ((System.Drawing.Image)(resources.GetObject("bunifuFlatButton1.Iconimage")));
+            this.bunifuFlatButton1.Iconimage_right = null;
+            this.bunifuFlatButton1.Iconimage_right_Selected = null;
+            this.bunifuFlatButton1.Iconimage_Selected = null;
+            this.bunifuFlatButton1.IconMarginLeft = 0;
+            this.bunifuFlatButton1.IconMarginRight = 0;
+            this.bunifuFlatButton1.IconRightVisible = true;
+            this.bunifuFlatButton1.IconRightZoom = 0D;
+            this.bunifuFlatButton1.IconVisible = true;
+            this.bunifuFlatButton1.IconZoom = 90D;
+            this.bunifuFlatButton1.IsTab = false;
+            this.bunifuFlatButton1.Location = new System.Drawing.Point(13, 225);
+            this.bunifuFlatButton1.Name = "bunifuFlatButton1";
+            this.bunifuFlatButton1.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.bunifuFlatButton1.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(129)))), ((int)(((byte)(77)))));
+            this.bunifuFlatButton1.OnHoverTextColor = System.Drawing.Color.White;
+            this.bunifuFlatButton1.selected = false;
+            this.bunifuFlatButton1.Size = new System.Drawing.Size(121, 48);
+            this.bunifuFlatButton1.TabIndex = 2;
+            this.bunifuFlatButton1.Text = "Dump";
+            this.bunifuFlatButton1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bunifuFlatButton1.Textcolor = System.Drawing.Color.White;
+            this.bunifuFlatButton1.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuFlatButton1.Click += new System.EventHandler(this.bunifuFlatButton1_Click);
+            // 
+            // logi
+            // 
+            this.logi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(63)))), ((int)(((byte)(70)))));
+            this.logi.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.logi.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.logi.ForeColor = System.Drawing.Color.White;
+            this.logi.Location = new System.Drawing.Point(396, 24);
+            this.logi.Name = "logi";
+            this.logi.ReadOnly = true;
+            this.logi.Size = new System.Drawing.Size(299, 195);
+            this.logi.TabIndex = 3;
+            this.logi.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.bunifuCustomLabel6);
+            this.groupBox1.Controls.Add(this.bunifuCustomLabel5);
+            this.groupBox1.Controls.Add(this.bunifuCustomLabel4);
+            this.groupBox1.Controls.Add(this.bunifuCustomLabel3);
+            this.groupBox1.Controls.Add(this.bunifuCustomLabel1);
+            this.groupBox1.Controls.Add(this.bunifuCustomLabel2);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.Location = new System.Drawing.Point(13, 75);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(376, 144);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tutorial";
+            // 
+            // bunifuCustomLabel6
+            // 
+            this.bunifuCustomLabel6.AutoSize = true;
+            this.bunifuCustomLabel6.ForeColor = System.Drawing.Color.Lime;
+            this.bunifuCustomLabel6.Location = new System.Drawing.Point(6, 38);
+            this.bunifuCustomLabel6.Name = "bunifuCustomLabel6";
+            this.bunifuCustomLabel6.Size = new System.Drawing.Size(358, 13);
+            this.bunifuCustomLabel6.TabIndex = 5;
+            this.bunifuCustomLabel6.Text = "2. type: print(GetCurrentServerEndpoint()) in yours lua executor to get servers ip.";
+            // 
+            // bunifuCustomLabel5
+            // 
+            this.bunifuCustomLabel5.AutoSize = true;
+            this.bunifuCustomLabel5.ForeColor = System.Drawing.Color.Lime;
+            this.bunifuCustomLabel5.Location = new System.Drawing.Point(6, 82);
+            this.bunifuCustomLabel5.Name = "bunifuCustomLabel5";
+            this.bunifuCustomLabel5.Size = new System.Drawing.Size(157, 13);
+            this.bunifuCustomLabel5.TabIndex = 4;
+            this.bunifuCustomLabel5.Text = "4. Wait until all files are fetched.";
+            // 
+            // bunifuCustomLabel4
+            // 
+            this.bunifuCustomLabel4.AutoSize = true;
+            this.bunifuCustomLabel4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bunifuCustomLabel4.ForeColor = System.Drawing.Color.White;
+            this.bunifuCustomLabel4.Location = new System.Drawing.Point(6, 128);
+            this.bunifuCustomLabel4.Name = "bunifuCustomLabel4";
+            this.bunifuCustomLabel4.Size = new System.Drawing.Size(360, 13);
+            this.bunifuCustomLabel4.TabIndex = 3;
+            this.bunifuCustomLabel4.Text = "GUI created by yunglean_#4171 Dumper created by marcodsl ";
+            // 
+            // bunifuCustomLabel3
+            // 
+            this.bunifuCustomLabel3.AutoSize = true;
+            this.bunifuCustomLabel3.ForeColor = System.Drawing.Color.Lime;
+            this.bunifuCustomLabel3.Location = new System.Drawing.Point(6, 101);
+            this.bunifuCustomLabel3.Name = "bunifuCustomLabel3";
+            this.bunifuCustomLabel3.Size = new System.Drawing.Size(235, 13);
+            this.bunifuCustomLabel3.TabIndex = 2;
+            this.bunifuCustomLabel3.Text = "5. Dumped files are in same directory as dumper.";
+            // 
+            // bunifuCustomLabel1
+            // 
+            this.bunifuCustomLabel1.AutoSize = true;
+            this.bunifuCustomLabel1.ForeColor = System.Drawing.Color.Lime;
+            this.bunifuCustomLabel1.Location = new System.Drawing.Point(6, 16);
+            this.bunifuCustomLabel1.Name = "bunifuCustomLabel1";
+            this.bunifuCustomLabel1.Size = new System.Drawing.Size(148, 13);
+            this.bunifuCustomLabel1.TabIndex = 0;
+            this.bunifuCustomLabel1.Text = "1. Start FiveM and join server.";
+            // 
+            // bunifuCustomLabel2
+            // 
+            this.bunifuCustomLabel2.AutoSize = true;
+            this.bunifuCustomLabel2.ForeColor = System.Drawing.Color.Lime;
+            this.bunifuCustomLabel2.Location = new System.Drawing.Point(6, 60);
+            this.bunifuCustomLabel2.Name = "bunifuCustomLabel2";
+            this.bunifuCustomLabel2.Size = new System.Drawing.Size(238, 13);
+            this.bunifuCustomLabel2.TabIndex = 1;
+            this.bunifuCustomLabel2.Text = "3. Provide IP and PORT and press Dump button.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(53)))), ((int)(((byte)(53)))));
+            this.ClientSize = new System.Drawing.Size(709, 281);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.logi);
+            this.Controls.Add(this.bunifuFlatButton1);
+            this.Controls.Add(this.port);
+            this.Controls.Add(this.ip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "yung X marcodsl FiveM Dumper";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuMetroTextbox ip;
+        private Bunifu.Framework.UI.BunifuMetroTextbox port;
+        private Bunifu.Framework.UI.BunifuFlatButton bunifuFlatButton1;
+        private System.Windows.Forms.RichTextBox logi;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel4;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel3;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel2;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel1;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel5;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel6;
+    }
+}
+
---
+++ Utilities/Resource-Dumper/dumper/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace dumper
+{
+    public partial class Form1 : Form
+    {
+        public static bool test = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Log(string text)
+        {
+            logi.Invoke(new MethodInvoker(delegate () { logi.AppendText(text + "\r\n"); logi.ScrollToCaret(); }));
+        }
+
+        private void bunifuFlatButton1_Click(object sender, EventArgs e)
+        {
+            logi.Text = "";
+            if (ip.Text == "IP")
+            {
+                Log("[!] Fill IP");
+            }
+            else if (port.Text == "PORT")
+            {
+                Log("[!] Fill PORT");
+            }
+            else if (port.Text == "")
+            {
+                Log("[!] Provide PORT");
+            }
+            else if (ip.Text == "")
+            {
+                Log("[!] Provide IP");
+            }
+            else if (port.Text == " ")
+            {
+                Log("[!] Provide PORT");
+            }
+            else if (ip.Text == " ")
+            {
+                Log("[!] Provide IP");
+            }
+            else
+            {
+                var path = new Uri(
+    System.IO.Path.GetDirectoryName(
+        System.Reflection.Assembly.GetExecutingAssembly().CodeBase)
+    ).LocalPath;
+                Process test = new Process();
+                test.StartInfo.FileName = "java";
+                test.StartInfo.UseShellExecute = false;
+                test.StartInfo.Arguments = "-jar dumper.jar -a " + ip.Text + " -p " + port.Text + " -o freedumper";
+                test.StartInfo.RedirectStandardOutput = false;
+                test.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+                string jd = @"\" + ip.Text;
+                try
+                {
+                    Log("[✔️] Dumping Started...");
+                    Log("[🔽] Files saving in: " + path + jd);
+                    test.Start();                    
+                }
+                catch (Exception ex)
+                {
+                    Log(ex.Message);
+                }               
+            }
+        }
+    }
+
+}
